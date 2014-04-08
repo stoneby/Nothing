ce3be6c19ec315d43a5e9556ae78936f5b9c3df4
cui xiaoliang <stonebyme@gmail.com>
2014-04-08 20:24:47 +0800

    go

---
+++ Assets/Game/Scripts/Template/TemplateWindowController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Specific window controller.
+/// </summary>
+public class TemplateWindowController : MonoBehaviour
+{
+    #region Window
+
+    public void OnOpen()
+    {
+    }
+
+    public void OnClose()
+    {
+    }
+
+    #endregion
+
+    #region Mono
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    #endregion
+}
